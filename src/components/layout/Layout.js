852e03026cb5ebcb860c6d51a9714fda2f3db18c
Big One <sponjjanc@naver.com>
2023-07-31 23:39:31 +0900

    add Layout(MainNav)

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,15 @@
+import styles from './Layout.module.css';
+import MainNavigation from "./MainNavigation";
+
+function Layout(props) {
+  return (
+      <>
+        <MainNavigation />
+        <main className={styles.main}>
+          {props.children}
+        </main>
+      </>
+  );
+}
+
+export default Layout;
