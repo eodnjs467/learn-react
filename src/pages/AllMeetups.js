b2d71f85d5b74ca993f668306775331c8d59c01f
Big One <sponjjanc@naver.com>
2023-08-01 23:27:39 +0900

    add loading, fetch get meetups

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,5 +1,46 @@
+import MeetupList from "../components/meetups/MeetupList";
+import {useEffect, useState} from "react";
+
 function AllMeetupsPage() {
-  return <div> All Meetups Page.</div>
+  const [loadMeetups, setLoadMeetups] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('https://react-meetup-15869-default-rtdb.firebaseio.com/meetups.json')
+        .then((res) => {
+          if(res.ok){
+            return res.json();
+          }
+        })
+        .then((data) => {
+          setLoading(false);
+          const meetups = [];
+          for(const key in data) {
+            const meetup = {
+              id: key,
+              ...data[key],
+            };
+            meetups.push(meetup);
+          }
+          setLoadMeetups(meetups);
+        })
+  }, []);
+
+  if (loading){
+    return(
+        <section>
+          <p>Loading...</p>
+        </section>
+    )
+  }
+
+  return (
+      <section>
+        <h1>All Meetups</h1>
+        <MeetupList meetups={loadMeetups}/>
+      </section>
+  );
 }
 
 export default AllMeetupsPage;
