d05b3335729fab132d6fb039014f7a58fd8cbf4d
Big One <sponjjanc@naver.com>
2023-08-03 11:13:28 +0900

    wrap context provider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import {BrowserRouter} from "react-router-dom";
+import {FavoritesContextProvider} from "./store/favorites-context";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <FavoritesContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </FavoritesContextProvider>
   </React.StrictMode>
 );
