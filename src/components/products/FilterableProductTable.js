3ca9ac60783205a79c82cda685302f60dfc0cb28
Big One <sponjjanc@naver.com>
2023-08-15 14:47:31 +0900

    moved folder and add input state, changeInput func

--- src/components/products/FilterableProductTable.js
+++ src/components/products/FilterableProductTable.js
@@ -3,12 +3,17 @@ import ProductTable from "./ProductTable";
 import {useCallback, useState} from "react";
 function FilterableProductTable(props) {
   const [checked, setChecked] = useState(false);
+  const [input, setInput] = useState('');
+
   const onChangeChecked = useCallback((e) => {
     setChecked(e.target.checked);
   }, []);
+  const onChangeInput = useCallback((e) => {
+    setInput(e.target.value);
+  }, [input]);
   return(
       <>
-        <SearchBar checked={checked} onChangeChecked={onChangeChecked} />
+        <SearchBar onChangeChecked={onChangeChecked} input={input} onChangeInput={onChangeInput} />
         <ProductTable data={props.data} filtered={checked} />
       </>
   )
